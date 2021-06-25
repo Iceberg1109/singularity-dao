a3d5e0e943c63aea6f06556c229427ffde577c9f
Iceberg1109 <smartdev951109@gmail.com>
2021-06-25 15:48:45 +0530

    exporting Add after adding layout

--- pages/pools/add/[...tokens].js
+++ pages/pools/add/[...tokens].js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from "react";
 import Admin from "layouts/Admin.js";
-import {  Col, Container, Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import Typography from "components/Typography";
 import AddLiquidityPanel from "../../../components/AddLiquidityPanel";
 import PoolInfoPanel from "components/PoolInfoPanel";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { abi as DynasetABI } from "../../../assets/constants/abi/Dynaset.json";
 import { useUser } from "components/UserContext";
 import { ethers } from "ethers";
 
-export default function Add() {
-  const router = useRouter()
-  const { tokens } = router.query
+function Add() {
+  const router = useRouter();
+  const { tokens } = router.query;
   const { library, account, network, chainId } = useUser();
   const [fromCurrency, setFromCurrency] = useState("ETH");
   const [toCurrency, setToCurrency] = useState("SDAO");
 
   const getCurrency = async (token) => {
     let currency;
-    if (token === '0xc778417e063141139fce010982780140aa0cd5ab') {
-      currency = 'ETH'
+    if (token === "0xc778417e063141139fce010982780140aa0cd5ab") {
+      currency = "ETH";
     } else {
       currency = await getTokenSymbol(token);
     }
 
-    return currency??'';
-  }
+    return currency ?? "";
+  };
 
   const getTokenSymbol = async (address) => {
     if (!account || !library) return;
@@ -38,17 +38,17 @@ export default function Add() {
   };
 
   const getSymbols = async (tokens) => {
-    if(tokens) {
-      const fromCurrency = await getCurrency(tokens[0])
-      const toCurrency = await getCurrency(tokens[1])
-      setFromCurrency(fromCurrency)
-      setToCurrency(toCurrency)
+    if (tokens) {
+      const fromCurrency = await getCurrency(tokens[0]);
+      const toCurrency = await getCurrency(tokens[1]);
+      setFromCurrency(fromCurrency);
+      setToCurrency(toCurrency);
     }
-  }
+  };
 
   useEffect(() => {
     getSymbols(tokens);
-  }, [tokens])
+  }, [tokens]);
 
   return (
     <Container className="my-4">
@@ -62,7 +62,7 @@ export default function Add() {
       <a>Learn more</a>
       <Row>
         <Col lg={7}>
-          <AddLiquidityPanel tokens={tokens}/>
+          <AddLiquidityPanel tokens={tokens} />
         </Col>
         <Col lg={5}>
           <PoolInfoPanel />
@@ -73,3 +73,5 @@ export default function Add() {
 }
 
 Add.layout = Admin;
+
+export default Add;
