2382c5c138086b41f6ec6579d8d92ad18ea4104f
Iceberg1109 <smartdev951109@gmail.com>
2021-06-28 13:52:27 +0530

    placeholder for AGIX-ETH

--- pages/pools/index.js
+++ pages/pools/index.js
@@ -6,55 +6,26 @@ import React, { useEffect, useState } from "react";
 
 //["0x5e94577b949a56279637ff74dfcff2c28408f049", "0xc778417e063141139fce010982780140aa0cd5ab"],
 
-// 1: ["0x993864e43caa7f7f12953ad6feb1d1ca635b875f", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"],
-//     3: ["0x5e94577b949a56279637ff74dfcff2c28408f049", "0xc778417e063141139fce010982780140aa0cd5ab"],
-// const baskets = [
-//   {
-//     poolId: "SDAO-ETH",
-//     apy: "13%",
-//     share: "0.005",
-//     tokens: {
-//       1: ["0x993864e43caa7f7f12953ad6feb1d1ca635b875f", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"],
-//       3: ["0x5e94577b949a56279637ff74dfcff2c28408f049", "0xc778417e063141139fce010982780140aa0cd5ab"],
-//     },
-//   },
-//   // {
-//   //   key: "WETH-AGIX",
-//   //   poolId: "AGIX-WETH",
-//   //   liquidity: "407004",
-//   //   apy: "4.82",
-//   //   share: "0.005",
-//   //   tokens: [
-//   //     "0x5e94577b949a56279637ff74dfcff2c28408f049",
-//   //     "0xa898150fef6ac506476e70f9bf1c03a11b55bdf9"
-//   //   ]
-//   // },
-// ];
+const baskets = [
+  {
+    poolId: "SDAO-ETH",
+    apy: "13%",
+    tokens: {
+      1: ["0x993864e43caa7f7f12953ad6feb1d1ca635b875f", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"],
+      3: ["0x5e94577b949a56279637ff74dfcff2c28408f049", "0xc778417e063141139fce010982780140aa0cd5ab"],
+    },
+  },
+  {
+    poolId: "AGIX-ETH",
+    apy: "4.82",
+    tokens: {
+      1: ["0x993864e43caa7f7f12953ad6feb1d1ca635b875f", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"],
+      3: ["0x5e94577b949a56279637ff74dfcff2c28408f049", "0xa898150fef6ac506476e70f9bf1c03a11b55bdf9"],
+    },
+  },
+];
 
 function ForgePage() {
-  const baskets = [
-    {
-      poolId: "SDAO-ETH",
-      apy: "13%",
-      share: "0.005",
-      tokens: {
-        1: ["0x993864e43caa7f7f12953ad6feb1d1ca635b875f", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"],
-        3: ["0x5e94577b949a56279637ff74dfcff2c28408f049", "0xc778417e063141139fce010982780140aa0cd5ab"],
-      },
-    },
-    // {
-    //   key: "WETH-AGIX",
-    //   poolId: "AGIX-WETH",
-    //   liquidity: "407004",
-    //   apy: "4.82",
-    //   share: "0.005",
-    //   tokens: [
-    //     "0x5e94577b949a56279637ff74dfcff2c28408f049",
-    //     "0xa898150fef6ac506476e70f9bf1c03a11b55bdf9"
-    //   ]
-    // },
-  ];
-
   return (
     <Container className="my-4">
       <Typography color="text1" size={32} weight={600}>
@@ -64,14 +35,14 @@ function ForgePage() {
         Select one of the liquidity pools available, then add liquidity and start to earn fees.
       </Typography>
       <div className="py-4 mt-5">
-      <Row className="my-3">
-        {baskets.map((basket) => (
-          <Col lg={4} key={basket.poolId}>
-            <ForgeBasket title={basket.poolId} apy={basket.apy} tokens={basket.tokens} />
-          </Col>
-        ))}
-      </Row>
-    </div>
+        <Row className="my-3">
+          {baskets.map((basket) => (
+            <Col lg={4} key={basket.poolId}>
+              <ForgeBasket title={basket.poolId} apy={basket.apy} tokens={basket.tokens} />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </Container>
   );
 }
